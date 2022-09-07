875339e9ec99233f74fca4412fdcde2c878bff91
João Victor Leoni <joaovictorleoni2021@gmail.com>
2022-09-07 13:15:58 -0300

    .

---
+++ exercicios/ex008.js
@@ -0,0 +1,7 @@
+var val = 65;
+console.log(`A velocidade de seu carro é ${val}km/h`);
+
+if(val > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança');
